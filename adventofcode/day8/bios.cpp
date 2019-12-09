385a0a407944144fdf93ba63170b9f74a5513577
Joshua <jschmidtnj@gmail.com>
2019-12-09 18:11:20 -0500

    finished advent. wasted time not realizing I had the right answer. need to work on that

--- adventofcode/day8/bios.cpp
+++ adventofcode/day8/bios.cpp
@@ -1,4 +1,8 @@
 #include <bits/stdc++.h>
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+
+using namespace cv;
 
 using namespace std;
 
@@ -19,22 +23,55 @@ int main()
   getline(thefile, line);
   istringstream iss;
   vector<pair<int, int>> count;
+  vector<vector<bool>> set;
+  set.reserve(height);
+  for (int i = 0; i < height; i++)
+  {
+    vector<bool> current;
+    current.reserve(width);
+    for (int j = 0; j < width; j++)
+      current.push_back(false);
+    set.push_back(current);
+  }
   iss.str(line);
   iss.clear();
-  for (int layerIndex = 0; layerIndex <  (int)line.size(); layerIndex += width * height) {
+  Mat image(height, width, CV_8UC3, Scalar(0, 0, 0));
+  for (int layerIndex = 0; layerIndex < (int)line.size(); layerIndex += width * height)
+  {
     int count0 = 0;
     int count1 = 0;
     int count2 = 0;
-    for (const char & currChar : line.substr(layerIndex, width * height))
-      if (currChar == '0')
-        count0++;
-      else if (currChar == '1')
-        count1++;
-      else if (currChar == '2')
-        count2++;
+    for (int row = 0; row < height; row++)
+    {
+      for (int col = 0; col < width; col++)
+      {
+        int i = layerIndex + row * width + col;
+        if (line[i] == '0')
+        {
+          count0++;
+          set[row][col] = true;
+        }
+        else if (line[i] == '1')
+        {
+          count1++;
+          if (!set[row][col])
+          {
+            image.at<Vec3b>(row, col)[0] = 255;
+            image.at<Vec3b>(row, col)[1] = 255;
+            image.at<Vec3b>(row, col)[2] = 255;
+          }
+          set[row][col] = true;
+        }
+        else if (line[i] == '2')
+        {
+          count2++;
+        }
+      }
+    }
     count.push_back(pair<int, int>(count0, count1 * count2));
   }
   sort(count.begin(), count.end(), comparePairs);
   cout << "num 0: " << count[0].first << endl;
   cout << "num 1 * 2: " << count[0].second << endl;
+  imwrite("image.png", image);
 }
