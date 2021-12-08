c9384883906dcfc497a4b9a3937eda05b0064c16
Joshua <jschmidtnj@gmail.com>
2021-12-08 01:40:35 -0500

    day6: fixed sol
    Signed-off-by: Joshua <jschmidtnj@gmail.com>

--- aoc2021/day6/solution.py
+++ aoc2021/day6/solution.py
@@ -23,45 +23,22 @@ def part_1() -> int:
     return len(fish)
 
 def part_2() -> int:
-    fish = np.array(0, dtype=np.ubyte)
-    with open('input.txt', 'r') as f:
-        for line in f:
-            fish = np.append(fish, [int(s) for s in line.split(',')])
+    data: List[int] = []
+    with open('input.txt') as f:
+        data = list(map(int, f.read().split(',')))
 
-    for curr_iter in range(256):
-        print(curr_iter)
-        to_append = 0
-        new_fish = np.array(0, dtype=np.ubyte)
-        for f in np.nditer(fish, op_flags=['readwrite']):
-            if f == 0:
-                new_fish = np.append(new_fish, 6)
-                to_append += 1
-            else:
-                new_fish = np.append(new_fish, f - 1)
-        new_fish = np.append(new_fish, np.full(to_append, 8, dtype=np.ubyte))
-        fish = new_fish
+    ages = [0] * 10
+    for i in data:
+        ages[i] += 1
 
+    for _ in range(256):
+        curr = ages[0]
+        for i in range(1, 9):
+            ages[i - 1] = ages[i]
+        ages[8] = curr
+        ages[6] += curr
 
-    return len(fish)
-
-def part_2() -> int:
-    fish = np.array(0, dtype=np.ubyte)
-    with open('input.txt', 'r') as f:
-        for line in f:
-            fish = np.append(fish, [int(s) for s in line.split(',')])
-
-    for curr_iter in range(256):
-        print(curr_iter)
-        to_append = 0
-        for i, f in enumerate(fish):
-            if f == 0:
-                fish[i] = 6
-                to_append += 1
-            else:
-                fish[i] -= 1
-        fish = np.append(fish, np.full(to_append, 8, dtype=np.ubyte))
-
-    return len(fish)
+    return sum(ages)
 
 if __name__ == '__main__':
     print(part_1())
